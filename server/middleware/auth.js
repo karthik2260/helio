29fc8fdf4e3f055e92e569baf8133a5c1b9b48bd
Karthik <karthik.dhanalekshmi@gmail.com>
2024-09-11 12:04:45 +0530

    googleauth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -14,7 +14,7 @@ require('dotenv').config(); // This should be at the top
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: 'http://localhost:7000/auth/google/callback'
+  callbackURL: 'http://www.helio.strangled.net/auth/google/callback'
 }, 
 async (accessToken, refreshToken, profile, cb) => {
   try {
